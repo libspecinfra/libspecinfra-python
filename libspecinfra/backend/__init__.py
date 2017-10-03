0392392b832a0a75a11e44a7cf6f3c01e7251da8
Marcy <marcy9114@gmail.com>
2017-10-03 14:08:19 +0900

    add SSH backend (not working yet)

--- libspecinfra/backend/__init__.py
+++ libspecinfra/backend/__init__.py
@@ -13,3 +13,15 @@ class Direct(object):
 
     def __exit__(self, exc_type, exc_value, traceback):
         self.lib.backend_direct_free(self.obj)
+
+class SSH(object):
+
+    def __init__(self, host):
+        lib = libspecinfra.load()
+        lib.backend_ssh_new.argtypes = (ctypes.c_char_p,)
+        lib.backend_ssh_new.restype = ctypes.POINTER(BackendWrapperS)
+        lib.backend_ssh_free.argtypes = (ctypes.POINTER(BackendWrapperS),)
+        self.obj = lib.backend_ssh_new(host.encode('utf-8'))
+
+    def __exit__(self, exc_type, exc_value, traceback):
+        self.lib.backend_ssh_free(self.obj)
